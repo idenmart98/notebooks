5c1c8b9cf6254ecb0c15dbfb5db17c4106817159
iden <idenmart07@gmail.com>
2020-08-14 21:06:02 +0600

    pip

---
+++ examples/test.py
@@ -0,0 +1,5 @@
+from maths.simplemath import add, div
+import vqarss
+
+
+print(vqarss.get_value())
---
+++ examples/vqarss.py
@@ -0,0 +1,11 @@
+value = 0
+
+
+def set_value(a):
+    global value
+    value = a
+    
+def get_value():
+    return value
+
+value = 0
