7c59f9743503223958d33a78be1c4ba73cd8c6ba
iden <idenmart07@gmail.com>
2020-08-27 21:56:18 +0600

    html

--- examples/test.py
+++ examples/test.py
@@ -1,5 +1,13 @@
-from maths.simplemath import add, div
-import vqarss
 
-
-print(vqarss.get_value())
+import random
+a = [1,2,3,4,5]
+def game():
+    b = 0
+    while b!=3:
+        b = b+1
+        choose = int(input('Число:'))
+        if random.choice(a) == choose:
+            print("you win")
+        else:
+            print("you lose")
+game()
